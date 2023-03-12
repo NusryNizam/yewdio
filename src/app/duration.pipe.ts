563557cf1932448e45e73db148e4575ef4992515
Nusry Nizam <nusrynizam@gmail.com>
2023-03-12 20:12:56 +0530

    Fix: duration and current seek time now have leading zeroes

--- src/app/duration.pipe.ts
+++ src/app/duration.pipe.ts
@@ -1,12 +1,14 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'duration'
+  name: "duration",
 })
 export class DurationPipe implements PipeTransform {
+  transform(value: number, type: "M" | "S"): string {
+    if (type === "M") return `${Math.floor(value / 60).toFixed(0)}`;
 
-  transform(value: number, ...args: unknown[]): string {
-    return `${(Math.floor(value / 60)).toFixed(0)}:${(value % 60).toFixed(0)}`
+    if (type === "S") return `${(value % 60).toFixed(0)}`;
+
+    return "";
   }
-
 }
