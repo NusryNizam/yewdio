0bec0ccabe00bdf40298890824933b8597691bb9
Nusry <nusrynizam@gmail.com>
2023-10-03 16:32:11 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     mode: 'Prod',
-    apiURL: 'https://vid.priv.au/api/v1/'
+    apiURL: 'https://anontube.lvkaszus.pl/api/v1/'
+    // apiURL: 'https://vid.priv.au/api/v1/'
 };
   
