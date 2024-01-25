a2375c93d6a4b5de09a99201bfa6e7c3301fa5b8
Nusry <nusrynizam@gmail.com>
2024-01-25 13:46:47 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
     mode: 'Prod',
-    apiURL: 'https://vid.priv.au/api/v1/'
+    apiURL: 'https://indivious.fdn.fr/api/v1'
+   // apiURL: 'https://vid.priv.au/api/v1/'
     // apiURL: 'https://anontube.lvkaszus.pl/api/v1/'
 };
   
